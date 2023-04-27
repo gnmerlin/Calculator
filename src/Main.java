2006bc2a31e601628335c35234e99fe230fe51cc
Iustin <philiustin@gmail.com>
2023-04-27 21:50:46 +0300

    The Sum is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the two numbers for addition:");
+        Scanner readNumber = new Scanner(System.in);
+        int a = readNumber.nextInt();
+        int b = readNumber.nextInt();
+        System.out.println("The sum of the numbers is " + Integer.sum(a, b));
+
     }
 }
